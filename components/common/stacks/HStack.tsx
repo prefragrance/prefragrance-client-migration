b8d3101e50d82f87767fac0027f80e44a0285e9b
jellysoo97 <jellysoo97@gmail.com>
2022-11-12 20:00:17 +0900

    fix: remove Avatar, add new stacks

--- components/common/stacks/HStack.tsx
+++ components/common/stacks/HStack.tsx
@@ -1,20 +1,66 @@
+import styled from "@emotion/styled";
 import React from "react";
-import { IFlex } from "../../../common/types/styles";
+
+type HStackProps = {
+  gap?: "none" | "xs" | "sm" | "md" | "lg";
+  align?: "flex-start" | "flex-end" | "space-between" | "center";
+  alignItems?: "stretch" | "flex-start" | "flex-end" | "center";
+  wrap?: "wrap" | "no-wrap" | "wrap-reverse";
+  padding?: string;
+  children: React.ReactNode;
+};
 
 const HStack = ({
-  wrap = "flex-wrap",
-  gap,
-  justifyContent = "justify-center",
-  alignItems = "items-center",
+  gap = "sm",
+  align = "center",
+  alignItems = "center",
+  wrap = "no-wrap",
+  padding = "0px",
   children,
-}: IFlex) => {
+}: HStackProps) => {
+  const getGap = () => {
+    switch (gap) {
+      case "none":
+        return "0rem";
+      case "xs":
+        return "0.5rem";
+      case "sm":
+        return "1rem";
+      case "md":
+        return "1.5rem";
+      case "lg":
+        return "2rem";
+      default:
+        throw new Error("invalid gap");
+    }
+  };
+
   return (
-    <div
-      className={`flex flex-row ${wrap} ${gap} ${justifyContent} ${alignItems}`}
+    <Container
+      align={align}
+      alignItems={alignItems}
+      wrap={wrap}
+      gap={getGap()}
+      padding={padding}
     >
       {children}
-    </div>
+    </Container>
   );
 };
+
+const Container = styled.div<{
+  gap: string;
+  align: string;
+  alignItems: string;
+  wrap: string;
+  padding: string;
+}>`
+  display: flex;
+  justify-content: ${({ align }) => align};
+  align-items: ${({ alignItems }) => alignItems};
+  flex-wrap: ${({ wrap }) => wrap};
+  gap: ${({ gap }) => gap};
+  padding: ${({ padding }) => padding};
+`;
 
 export default HStack;
--- components/common/stacks/VStack.tsx
+++ components/common/stacks/VStack.tsx
@@ -1,20 +1,71 @@
+import styled from "@emotion/styled";
 import React from "react";
-import { IFlex } from "../../../common/types/styles";
+
+type VStackProps = {
+  gap?: "none" | "xs" | "sm" | "md" | "lg";
+  align?:
+    | "flex-start"
+    | "flex-end"
+    | "center"
+    | "space-between"
+    | "space-evenly";
+  alignItems?: "stretch" | "flex-start" | "flex-end" | "center";
+  wrap?: "wrap" | "no-wrap" | "wrap-reverse";
+  padding?: string;
+  children: React.ReactNode;
+};
 
 const VStack = ({
-  wrap = "flex-wrap",
-  gap,
-  justifyContent = "justify-center",
-  alignItems = "items-center",
+  align = "center",
+  alignItems = "center",
+  wrap = "no-wrap",
+  gap = "sm",
+  padding = "0px",
   children,
-}: IFlex) => {
+}: VStackProps) => {
+  const getGap = () => {
+    switch (gap) {
+      case "none":
+        return "0rem";
+      case "xs":
+        return "0.5rem";
+      case "sm":
+        return "1rem";
+      case "md":
+        return "1.5rem";
+      case "lg":
+        return "2rem";
+      default:
+        throw new Error("invalid gap");
+    }
+  };
+
   return (
-    <div
-      className={`flex flex-row ${wrap} ${gap} ${justifyContent} ${alignItems}`}
+    <Container
+      align={align}
+      alignItems={alignItems}
+      wrap={wrap}
+      gap={getGap()}
+      padding={padding}
     >
       {children}
-    </div>
+    </Container>
   );
 };
+
+const Container = styled.div<{
+  gap: string;
+  align: string;
+  alignItems: string;
+  wrap: string;
+  padding: string;
+}>`
+  display: flex;
+  justify-content: ${({ align }) => align};
+  align-items: ${({ alignItems }) => alignItems};
+  flex-wrap: ${({ wrap }) => wrap};
+  gap: ${({ gap }) => gap};
+  padding: ${({ padding }) => padding};
+`;
 
 export default VStack;
