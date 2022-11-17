6cf855c749153be719f1267fa7a206cc360ae180
zizonyoungjun <naya700@naver.com>
2022-11-17 16:58:18 +0900

    feat: SearchNavBar

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,18 +1,30 @@
 import styled from "@emotion/styled";
 import React from "react";
 import Navbar from "./Navbar";
+import SearchNavbar from "./SearchNavbar";
+import { useRouter } from "next/router";
 
 interface ILayout {
   children: React.ReactNode;
 }
 
 const Layout = ({ children }: ILayout) => {
-  return (
-    <>
-      <Navbar />
-      <PageContainer>{children}</PageContainer>
-    </>
-  );
+  const router = useRouter();
+  if (router.route == "/") {
+    return (
+      <>
+        <Navbar />
+        <PageContainer>{children}</PageContainer>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <SearchNavbar />
+        <PageContainer>{children}</PageContainer>
+      </>
+    );
+  }
 };
 
 const PageContainer = styled.section`
