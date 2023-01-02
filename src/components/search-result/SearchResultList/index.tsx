a25f93f3591d1f058316ca899752058615b7feb0
HiImConan <mindol0320@naver.com>
2023-01-02 17:59:00 +0900

    style: integrate searchTab and searchResultList and styling components

--- src/components/search-result/SearchResultList/index.tsx
+++ src/components/search-result/SearchResultList/index.tsx
@@ -1,35 +1,68 @@
 import React from "react";
 import styled from "@emotion/styled";
-import { fontSize, palette } from "@src/styles/styles";
-import SearchResultBlock from "./SearchResultBlock";
+import { fontSize, fontWeight, palette } from "@src/styles/styles";
+import SearchResultItem from "./SearchResultItem";
 import { ISearchResult } from "@src/common/types/search";
+import Select from "@src/components/common/select/Select";
+import { HStack, VStack } from "@common-components";
+
+export interface IOrderProps {
+  label: string;
+  value: string;
+}
+
+const orderList: IOrderProps[] = [
+  { label: "조회순", value: "" },
+  { label: "별점순", value: "" },
+  { label: "리뷰 많은 순", value: "" },
+];
 
 const SearchResultList = ({
   searchResult,
 }: {
   searchResult: ISearchResult;
 }) => {
+  const searchResultCount = Object(searchResult).length | 0;
   return (
-    <ListWrapper>
-      {Object(searchResult).length > 0 ? (
-        Object(searchResult).map((result: ISearchResult) => (
-          <SearchResultBlock key={result.id} data={result} />
-        ))
-      ) : (
-        <NotFound>알맞은 검색 결과를 찾지 못하였습니다.</NotFound>
-      )}
-    </ListWrapper>
+    <Wrapper>
+      <InfoWrapper align="space-between" padding="0rem 0.5rem">
+        <span>검색결과: 총 {searchResultCount}개</span>
+        <Select optionList={orderList} value={""} />
+      </InfoWrapper>
+      <ListWrapper>
+        {Object(searchResult).length > 0 ? (
+          Object(searchResult).map((result: ISearchResult) => (
+            <SearchResultItem key={result.id} data={result} />
+          ))
+        ) : (
+          <NotFound>원하는 검색 결과를 찾지 못했습니다.</NotFound>
+        )}
+      </ListWrapper>
+    </Wrapper>
   );
 };
 
+const Wrapper = styled(VStack)`
+  width: 70vw;
+`;
+
 const ListWrapper = styled.div`
-  width: 1200px;
+  width: 100%;
   height: auto;
-  min-height: 100%; // 세로 최솟값 더 정확하게 연동시키는 방법?
+  // min-height: 100%; // 세로 최솟값 더 정확하게 연동시키는 방법?
   display: flex;
   flex-direction: column;
   overflow-y: scroll;
-  background-color: ${palette.gray.light};
+  & > div:not(:last-child) {
+    border-bottom: 1px solid ${palette.gray.dark};
+  }
+`;
+
+const InfoWrapper = styled(HStack)`
+  width: 100%;
+  span {
+    font-weight: ${fontWeight.semiBold};
+  }
 `;
 
 const NotFound = styled.div`
@@ -37,7 +70,7 @@ const NotFound = styled.div`
   justify-content: center;
   align-items: center;
   font-size: ${fontSize.mediumTitle}
-  background: ${palette.gray}
+  border: 1px solid ${palette.gray}
   width: 100%;
   height: 50vh;
 `;
