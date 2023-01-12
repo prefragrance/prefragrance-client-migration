08cf6e5043701e95ec3f9f7fc97e2b43118f1ff0
zizonyoungjun <naya700@naver.com>
2023-01-12 15:23:53 +0900

    fix: ui 수정

--- src/components/magazine/useMagazineBox.tsx
+++ src/components/magazine/useMagazineBox.tsx
@@ -2,9 +2,17 @@ import React from "react";
 import styled from "@emotion/styled";
 import { IMagazineProduct } from "@src/common/types/magazine";
 import Link from "next/link";
-import { palette } from "@src/styles/styles";
+import { fontSize, palette, fontWeight } from "@src/styles/styles";
 import { useProductReview } from "@src/components/product/useProductReview";
 import { RouterUrl } from "@src/common/constants/path";
+import { Icon } from "@src/components/common";
+import { calculateSize } from "@src/common/utils/calculateSize";
+
+interface ITag {
+  content: string;
+  backgroundColor?: string;
+  color?: string;
+}
 
 const MagazineBox = ({
   id,
@@ -22,26 +30,41 @@ const MagazineBox = ({
       <Img src={thumbnail_url} />
       <TitleRateSection>
         <Name>{name}</Name>
+      </TitleRateSection>
+      <TitleRateSection>
+        <CraftmanImg src="/assets/images/craftman.png" />
+        <Producer>{producer}</Producer>
+        <Star>
+          <Icon size={`${fontSize.bigTitle}`} color={`${palette.tags.yellow}`}>
+            star
+          </Icon>
+        </Star>
         <Rate>{rate}</Rate>
       </TitleRateSection>
-      <InfoSection>
-        <Producer>{producer}</Producer>
-      </InfoSection>
+      <Tag content="Top Review" />
       <ProfileImg src="/assets/images/blank-profile.png" />
       {productReview[0]?.content.length === 0 && (
-        <Content>너무 좋은 향이에요 !!</Content>
+        <Content>&quot; 너무 좋은 향이에요 !! &quot;</Content>
       )}
       {productReview[0]?.content.length > 0 && (
-        <Content>{productReview[0]?.content}</Content>
+        <Content>&quot; {productReview[0]?.content} &quot;</Content>
       )}
       <InfoSection>
         <ToDetail>
           <Link href={`${RouterUrl.ProductDetail}/${id}`}>
-            <a>제품 자세히 보기</a>
+            <a>제품 자세히 보기 ➤➤ </a>
           </Link>
         </ToDetail>
       </InfoSection>
     </ReviewSection>
+  );
+};
+
+const Tag = ({ content, backgroundColor, color }: ITag) => {
+  return (
+    <ReviewBox backgroundColor={backgroundColor} color={color}>
+      {content}
+    </ReviewBox>
   );
 };
 
@@ -70,6 +93,14 @@ const ProfileImg = styled.img`
   margin-right: 1rem;
 `;
 
+const CraftmanImg = styled.img`
+  width: 2rem;
+  height: 2rem;
+  float: left;
+  margin-right: 0.5rem;
+  margin-top: 0.1rem;
+`;
+
 const InfoSection = styled.div`
   display: flex;
   align-items: left;
@@ -85,14 +116,16 @@ const TitleRateSection = styled.div`
 const Content = styled.div`
   word-wrap: break-word;
   text-overflow: ellipsis;
-  margin-top: 1rem;
+  margin-top: 1.5rem;
+  font-weight: 300;
+  font-size: 1.2rem;
 `;
 
 const Name = styled.div`
   font-size: 3rem;
   font-weight: 700;
   margin-top: 4.5rem;
-  margin-bottom: 1rem;
+  margin-bottom: 1.2rem;
 `;
 
 const Producer = styled.div`
@@ -101,18 +134,24 @@ const Producer = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-bottom: 2.5rem;
-  margin-left: 2.5rem;
+  margin-bottom: 3rem;
+  margin-top: 0.3rem;
 `;
 
 const Rate = styled.div`
   font-size: 2rem;
   font-weight: 500;
-  margin-top: 5.3rem;
-  margin-left: 2rem;
+  margin-left: 0.5rem;
   align-items: center;
   justify-content: center;
-  color: ${palette.green.rightLogo};
+  color: ${palette.tags.yellow};
+`;
+const Star = styled.div`
+  margin-top: 0.4rem;
+  margin-left: 1.5rem;
+  align-items: center;
+  justify-content: center;
+  color: ${palette.tags.yellow};
 `;
 
 const ToDetail = styled.div`
@@ -121,8 +160,23 @@ const ToDetail = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  margin-top: 10rem;
+  margin-top: 6rem;
   margin-left: 35rem;
+`;
+
+const ReviewBox = styled.div<Pick<ITag, "backgroundColor" | "color">>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${({ backgroundColor = palette.green.rightLogo }) =>
+    backgroundColor};
+  color: ${({ color = palette.green.primary }) => color};
+  width: 96px;
+  height: 32px;
+  border-radius: 6px;
+  font-weight: ${fontWeight.semiBold};
+  font-size: ${calculateSize(14)};
+  margin-bottom: 1rem;
 `;
 
 export default MagazineBox;
